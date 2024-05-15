3f30f3bccfeb7ee75566ea3400aa97fde69c2c8c
Sofia Landaveri <sofialandaveri27@gmail.com>
2024-05-14 23:32:15 -0300

    modificaciones css pag principal

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,9 +9,9 @@ export const NavBar = () => {
                 <img src="/src/assets/RyM.png" alt="asd" className="logo-img"/>
             </div>
             <div className="nav-links">
-                <Link to="/characters" className="link">Characters</Link>
-                <Link to="/locations" className="link">Location</Link>
-                <Link to="/episodes" className="link">Episode</Link>
+                <Link to="/characters" className="link">CHARACTERS</Link>
+                <Link to="/locations" className="link">LOCATION</Link>
+                <Link to="/episodes" className="link">EPISODE</Link>
             </div>
 
         </nav>
--- src/pages/Presentation.jsx
+++ src/pages/Presentation.jsx
@@ -9,17 +9,17 @@ export const Presentation = () => {
             <div className="About">
                 <div className="About-Text">
                     <h2 className="About-Text title">About the Project</h2>
-                <p>
+                <p className="Text">
                     This project was created with the purpose of exploring a bit of the history of the series Rick And Morty. Here you can discover information about its characters, episodes, and locations.
                 </p>
-                <p>
+                <p className="Text">
                     Explore our application to immerse yourself in the Rick and Morty universe and discover more about the characters, episodes, and locations that make this series a cultural phenomenon.
                 </p>
-                <p>
+                <p className="Text">
                     Get ready for an adventure filled with humor, action, and reflection!
                 </p>
             </div>
-                <h2 className="About-Text title">See the intro</h2>
+                <h2 className="About-Text title video">See the intro</h2>
                 <div className="About-Video">
                 <iframe
                     width="660" // Ancho del reproductor de video
