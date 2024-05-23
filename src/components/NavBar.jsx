1bd353050e133cc0c52fe02682bec6a73e57fcc6
enluuca <giangreco.en@gmail.com>
2024-05-23 16:05:13 -0300

    f

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../style/NavBar.css"
+import icon from '/src/assets/icon2.png';
 
 export const NavBar = () => {
     return (
         <nav className="navBar">
             <div className="logo">
-                <img src="/assets/icon.png" alt="Rick and Morty Logo" title="Rick and Morty Logo" className="logo-img"/>
+                <img src={icon}alt="Rick and Morty Logo" title="Rick and Morty Logo" className="logo-img"/>
             </div>
             <div className="nav-links">
                 <Link to="/characters" className="link">CHARACTERS</Link>
